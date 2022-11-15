dccbf1ccd651c79c10c33339b62c18adb38b4a72
AshfaqAliKashif <44152910+AshfaqAliKashif@users.noreply.github.com>
2022-11-15 12:16:59 +0500

    Update settings.py

--- QimsCms/QimsCms/settings.py
+++ QimsCms/QimsCms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uy#d_)33^pconbazrq_gjl-jfuil28&i4(_6ivv4talunq4vik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
